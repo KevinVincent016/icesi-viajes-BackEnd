e065b11210c05f31ee26c968758fdea4f300b171
John Kennedy Landazuri Sandoval <jkls1998@gmail.com>
2024-06-05 01:05:39 -0500

    Planes endpoints

--- src/main/java/co/edu/icesi/viajes/dto/PlanDTO.java
+++ src/main/java/co/edu/icesi/viajes/dto/PlanDTO.java
@@ -1,9 +1,16 @@
 package co.edu.icesi.viajes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlanDTO implements Serializable{
     
 
@@ -25,27 +32,6 @@ public class PlanDTO implements Serializable{
     private ClienteDTO cliente;
     private UsuarioDTO usuario;
 
-    public PlanDTO(Integer idPlan, String codigo, String descripcionSolicitud, String nombre, Integer cantidadPersonas,
-                   Date fechaSolicitud, Date fechaInicioViaje, Date fechaFinViaje, BigDecimal valorTotal, Date fechaCreacion,
-                   Date fechaModificacion, String usuCreador, String usuModificador, String estado, ClienteDTO cliente,
-                   UsuarioDTO usuario) {
-        this.idPlan = idPlan;
-        this.codigo = codigo;
-        this.descripcionSolicitud = descripcionSolicitud;
-        this.nombre = nombre;
-        this.cantidadPersonas = cantidadPersonas;
-        this.fechaSolicitud = fechaSolicitud;
-        this.fechaInicioViaje = fechaInicioViaje;
-        this.fechaFinViaje = fechaFinViaje;
-        this.valorTotal = valorTotal;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaModificacion = fechaModificacion;
-        this.usuCreador = usuCreador;
-        this.usuModificador = usuModificador;
-        this.estado = estado;
-        this.cliente = cliente;
-        this.usuario = usuario;
-    }
 
 
     
